9f54a815d8d93bc5be27c84d9941a91447f7833b
tempname1533 <pedro_de_sousa@outlook.com>
2020-05-09 10:42:21 +0200

    todo list

--- App.js
+++ App.js
@@ -1,19 +1,80 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import colors from "./Colors";
+import { AntDesign } from "@expo/vector-icons";
+import tempData from "./tempData";
+import Todo from "./components/Todo";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flexDirection: "row" }}>
+        <View style={styles.divider} />
+        <Text style={styles.title}>
+          Todo
+          <Text style={{ fontWeight: "300", color: colors.blue }}>Lists</Text>
+        </Text>
+        <View style={styles.divider} />
+      </View>
+      <View style={{ marginVertival: 48 }}>
+        <TouchableOpacity style={styles.addList}>
+          <AntDesign name="plus" size={15} color={colors.blue} />
+        </TouchableOpacity>
+        <Text style={styles.add}>Add List</Text>
+      </View>
+      <View style={{ height: 275, paddingLeft: 32 }}>
+        <FlatList
+          data={tempData}
+          keyExtractor={(item) => item.name}
+          horizontal={true}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <Todo list={item} />}
+        />
+      </View>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
   },
 });
---
+++ tempData.js
@@ -0,0 +1,24 @@
+const tempData = [
+  {
+    name: "Plan a trip",
+    color: "#24A6D9",
+    todos: [
+      { title: "Book flight", completed: false },
+      { title: "Book flight", completed: true },
+      { title: "Book flight", completed: false },
+      { title: "Book flight", completed: false },
+    ],
+  },
+  {
+    name: "Plan a trip2",
+    color: "#8022d9",
+    todos: [
+      { title: "Book flight", completed: false },
+      { title: "Book flight", completed: true },
+      { title: "Book flight", completed: false },
+      { title: "Book flight", completed: false },
+    ],
+  },
+];
+
+export default tempData;
